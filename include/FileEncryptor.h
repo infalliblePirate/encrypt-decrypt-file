5c3177e52774213a089c5af029c78de4e20c9101
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2024-01-13 23:23:37 +0200

    created encrypt logic

--- include/FileEncryptor.h
+++ include/FileEncryptor.h
@@ -1,12 +1,14 @@
 #ifndef FILEENCRYPTOR_H
 #define FILEENCRYPTOR_H
 
+#include <string>
 
+class FileEncryptor {
+public:
+  static std::string encrypt(const std::string& source_path);
 
-class FileEcryptor {
-
+  static std::string decrypt(const std::string& encrypted_path);
 };
-
 
 
 #endif //FILEENCRYPTOR_H
--- src/FileEncryptor.cpp
+++ src/FileEncryptor.cpp
@@ -1,1 +1,26 @@
 #include "../include/FileEncryptor.h"
+#include <fstream>
+
+std::string FileEncryptor::encrypt(const std::string& source_path) {
+  int key = 5;
+
+  std::fstream fin, fout;
+  fin.open(source_path, std::fstream::in);
+  fout.open("encrypted.txt", std::fstream::out);
+
+  std::string encrypted_txt;
+  char c;
+  while(fin >> c) {
+    int temp = c + key;
+
+    fout<< static_cast<char>(temp);
+    encrypted_txt += static_cast<char>(temp);
+  }
+
+  return encrypted_txt;
+}
+
+std::string FileEncryptor::decrypt(const std::string &encrypted_path) {
+
+  return "";
+}
