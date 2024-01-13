0b87a974bdfa567e63c73090a9dcec7cf7364d65
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2024-01-13 23:01:06 +0200

    added tests

---
+++ tests/test_FileEncryptor.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "../include/FileEncryptor.h"
+
+#include <string>
+
+TEST(FileEncryptor, Encryption) {
+    const std::string source_path = "../data/encrypt.txt";
+    // key is 5;
+    EXPECT_EQ(FileEncryptor::encrypt(source_path), "Mjqqt%Rjmgy!");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
