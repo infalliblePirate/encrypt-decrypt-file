b87ed943a9e8078ebc240463123ab05626282408
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2024-01-14 11:44:43 +0200

    added tests

--- tests/test_FileEncryptor.cpp
+++ tests/test_FileEncryptor.cpp
@@ -4,9 +4,15 @@
 #include <string>
 
 TEST(FileEncryptor, Encryption) {
-    const std::string source_path = "../data/encrypt.txt";
+    const std::string source_path = "../data/text.txt";
     // key is 5;
     EXPECT_EQ(FileEncryptor::encrypt(source_path), "Mjqqt\\twqi&");
+}
+
+TEST(FileEncryptor, Decrypt) {
+    const std::string encrypted_path = "../data/encrypted.txt";
+    // key is 5
+    EXPECT_EQ(FileEncryptor::decrypt(encrypted_path), "HelloWorld!");
 }
 
 int main(int argc, char **argv) {
