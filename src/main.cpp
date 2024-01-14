45a7ef3ca7ab2bee4b7071f1cedc5401c427e98a
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2024-01-14 18:29:26 +0200

    implemented encryption with 1-number key

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "../include/FileEncryptor.h"
 
-int main() {
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cout << "Example: ./encryptor path_to_file key (1 - encrypt || 0 - decrypt)" << std::endl;
+    return -1;
+  }
 
+  std::string source_path = argv[1];
+  int key = atoi(argv[2]);
+  int encryption_flag = atoi(argv[3]);
+
+  if (encryption_flag) {
+    std::cout << FileEncryptor::encrypt(source_path, key) << std::endl;
+  } else {
+    std::cout << FileEncryptor::decrypt(source_path, key) << std::endl;
+  }
+
+  return 0;
 }
