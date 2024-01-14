cef0189d22230abe9a1e36eb5a78f450343f7708
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2024-01-14 11:35:44 +0200

    added decryption logic

--- src/FileEncryptor.cpp
+++ src/FileEncryptor.cpp
@@ -1,26 +1,44 @@
 #include "../include/FileEncryptor.h"
 #include <fstream>
 
-std::string FileEncryptor::encrypt(const std::string& source_path) {
+std::string FileEncryptor::encrypt(const std::string &source_path) {
   int key = 5;
 
   std::fstream fin, fout;
   fin.open(source_path, std::fstream::in);
-  fout.open("encrypted.txt", std::fstream::out);
+  fout.open("../data/encrypted.txt", std::fstream::out);
 
   std::string encrypted_txt;
   char c;
-  while(fin >> c) {
+  while (fin >> c) {
     int temp = c + key;
 
-    fout<< static_cast<char>(temp);
+    fout << static_cast<char>(temp);
     encrypted_txt += static_cast<char>(temp);
   }
 
+  fin.close();
+  fout.close();
   return encrypted_txt;
 }
 
 std::string FileEncryptor::decrypt(const std::string &encrypted_path) {
+  int key = 5;
 
-  return "";
+  std::fstream fin, fout;
+  fin.open("../data/encrypted.txt", std::fstream::in);
+  fout.open("decrypted.txt", std::fstream::out);
+
+  std::string decrypted_txt;
+  char c;
+  while(fin >> c) {
+    int temp = c - key;
+
+    fout << static_cast<char>(temp);
+    decrypted_txt += static_cast<char>(temp);
+  }
+
+  fin.close();
+  fout.close();
+  return decrypted_txt;
 }
